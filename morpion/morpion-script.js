0c588ac9f312c4bd597e741a70697ddd1c686780
Pierre <pierre.treussart@gmail.com>
2022-03-23 00:41:52 +0100

    sound + aesthetics

--- morpion/morpion-script.js
+++ morpion/morpion-script.js
@@ -1,16 +1,19 @@
 const LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
-const CASES = document.querySelectorAll(".case");
+const BOXES = document.querySelectorAll(".box");
 const RESET_BUTTON = document.querySelector(".reset-button");
 const TEXT = document.querySelector("h1");
+
 var victoire = false;
 var joueur = "";
 
+function playSound(soundfile){
+    document.getElementById("sound").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\"/>";
+}
 function start() {
-    CASES.forEach(btn => {
-        btn.innerText = "";
-        btn.classList. add("clickable");
-        btn.classList.remove("highlighted");
-        btn.classList.remove("clicked");
+    BOXES.forEach(box => {
+        box.innerText = "";
+        box.classList. add("clickable");
+        box.classList.remove("highlighted");
     })
     RESET_BUTTON.classList.remove("showing");
     TEXT.innerText = ""
@@ -23,9 +26,9 @@ function end() {
 }
 function checkWin() {
     for (var i = 0; i < LINES.length; i++) {
-        var check = CASES[LINES[i][0] - 1].innerText
-                    + CASES[LINES[i][1] - 1].innerText
-                    + CASES[LINES[i][2] - 1].innerText;
+        var check = BOXES[LINES[i][0] - 1].innerText
+                    + BOXES[LINES[i][1] - 1].innerText
+                    + BOXES[LINES[i][2] - 1].innerText;
         if (check == "XXX") {
             TEXT.innerText = "Victoire du joueur X !";
             victoire = true;
@@ -34,9 +37,9 @@ function checkWin() {
             victoire = true;
         }
         if (victoire) {
-            CASES[LINES[i][0] - 1].classList.add("highlighted");
-            CASES[LINES[i][1] - 1].classList.add("highlighted");
-            CASES[LINES[i][2] - 1].classList.add("highlighted");
+            BOXES[LINES[i][0] - 1].classList.add("highlighted");
+            BOXES[LINES[i][1] - 1].classList.add("highlighted");
+            BOXES[LINES[i][2] - 1].classList.add("highlighted");
             end();
             return true;
         }
@@ -45,24 +48,24 @@ function checkWin() {
 }
 function checkFull() {
     for (var i = 0; i < LINES.length; i++) {
-        if (CASES[i].innerText == "") {
+        if (BOXES[i].innerText == "") {
             return;
         }
     }
     TEXT.innerText = "Partie nulle.";
     end();
 }
-function onClick(btn) {
-    if (btn.innerText == "") {
+function onClick(box) {
+    if (box.innerText == "") {
+        playSound("Pop.mp3")
         if (joueur !== "") { 
-            btn.innerText = joueur;
+            box.innerText = joueur;
             if (joueur == "X") {
                 joueur = "O";
             } else {
                 joueur = "X";
             };
-            btn.classList.remove("clickable");
-            btn.classList.add("clicked");
+            box.classList.remove("clickable");
             console.log("Au tour du joueur " +  joueur + ".");
             checkWin();
             if (!victoire) {
@@ -72,13 +75,14 @@ function onClick(btn) {
     }
 }
 
-CASES.forEach(btn => {
-    btn.addEventListener("click", () => {
-        onClick(btn);
+BOXES.forEach(box => {
+    box.addEventListener("click", () => {
+        onClick(box);
     })
 })
 RESET_BUTTON.addEventListener("click", () => {
     start();
 })
+
 
 start();
